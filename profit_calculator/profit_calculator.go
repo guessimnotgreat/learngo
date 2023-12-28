fedeca83dbb2f5bba67128d2470f998471e8c232
guessimnotgreat <rit.yotsathon@gmail.com>
2023-12-28 23:57:17 +0700

    finish profit calculator

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -5,23 +5,27 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
-	gettingInput("Revenue", &revenue)
-	gettingInput("Expenses", &expenses)
-	gettingInput("Tax Rate", &taxRate)
+	revenue := gettingInput("Revenue")
+	expenses := gettingInput("Expenses")
+	taxRate := gettingInput("Tax Rate")
 
+	ebt, profit, ratio := calculateFinancial(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+}
+
+func gettingInput(input string) float64 {
+	var result float64
+	fmt.Printf("%s: ", input)
+	fmt.Scan(&result)
+	return result
+}
+
+func calculateFinancial(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
-
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
-}
-
-func gettingInput(input string, result *float64) {
-	fmt.Printf("%s: ", input)
-	fmt.Scan(result)
+	return ebt, profit, ratio
 }
