369c363a67c942c876fbd242fc8da2734cbea04e
guessimnotgreat <rit.yotsathon@gmail.com>
2023-12-29 16:12:30 +0700

    implement menu condition

--- bank_project/bank.go
+++ bank_project/bank.go
@@ -12,24 +12,71 @@ const (
 )
 
 func main() {
-	printMenu(
+
+	accountBalance := 1000.00
+
+	printMenu()
+
+	for {
+		choice := getUserChoice()
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is", accountBalance)
+		case 2:
+			var depositAmount float64
+			fmt.Print("Your deposit: ")
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0")
+				return
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		case 3:
+			var withdrawAmount float64
+			fmt.Print("Your withdraw: ")
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0")
+				return
+			}
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than you have.")
+				return
+			}
+
+			accountBalance -= withdrawAmount
+			fmt.Println("Balance updated! New amount:", accountBalance)
+		default:
+			fmt.Println("Goodbye!")
+			return
+		}
+	}
+
+}
+
+func printMenu() {
+	items := []string{
 		WelcomeMessage,
 		PromptMessage,
 		CheckBalance,
 		DepositMoney,
 		WithdrawMoney,
 		ExitMessage,
-	)
+	}
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
-
-	fmt.Println("Your choice:", choice)
-}
-
-func printMenu(items ...string) {
 	for _, item := range items {
 		fmt.Println(item)
 	}
+}
+
+func getUserChoice() int {
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+	return choice
 }
